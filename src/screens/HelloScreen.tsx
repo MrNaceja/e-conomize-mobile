3e27f9415af789301b03f98893a17deee5457dd5
Naceja <edutoriani13@gmail.com>
2023-05-16 16:51:00 -0300

    declarações de tipagem do resolver

--- src/screens/HelloScreen.tsx
+++ src/screens/HelloScreen.tsx
@@ -1,9 +1,14 @@
 import { VStack } from "native-base";
 
+import Logo from '@assets/logo-economize.svg'
+
+/**
+ * Tela de Boas Vindas do App.
+ */
 export default function HelloScreen() {
     return (
         <VStack bg="gray.200">
-            
+            <Logo />
         </VStack>
     )
 }
